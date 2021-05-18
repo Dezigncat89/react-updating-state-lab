f6c3fc337df3d836047f60e93c55eb267a82ef79
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-18 16:56:25 -0700

    Update DigitalClicker.js

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,32 @@
-// Code DigitalClicker Component Here
+import React from "react"
+
+class DigitalClicker extends React.Component {
+  constructor() {
+     super()
+     this.state = {
+     timesClicked: 0
+     }
+     this.handleClicked = this.handleClicked.bind(this)
+   }
+
+   handleClicked() {
+     this.setState((prevState => {
+         return {
+           timesClicked: preState.timesClicked + 1
+         }
+     })
+  }
+
+render() {
+  return (
+    <div>
+      <h1>{this.state.timesClicked}</h1>
+      <button onClicked={this.handleClicked}>Start Count</button>
+    </div>
+    )
+  }
+}
+
+{<YouTubeDebugger/>}
+
+export default DigitalClicker
