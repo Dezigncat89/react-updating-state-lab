06154ef91109956dfb9f5d4e2e49de1683a7b40f
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-18 17:36:52 -0700

    Update YouTubeDebugger.js

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,52 @@
-// Code YouTubeDebugger Component Here
+import React, {Component} from "react"
+
+class YoutubeDebugger extends Component {
+  constructor() {
+    super()
+    this.state = {
+       errors: [],
+       user: null,
+  settings: {
+  bitrate: 8,
+  video: {
+  resolution: '1080p'
+  }
+  this.handleChange = this.handleChange.bind(this)
+}
+
+ handleClick() {
+   this.setState((prevState => {
+     return {
+       bitrate: preState.bitrate + 4
+     }
+
+   })
+}
+render() {
+  return (
+    <div>
+       <h1>{console.log(this.props.bitrate}</h1>
+       <button onClick={this.handleClick}>Current Value</button>
+    </div>
+   )
+   }
+}
+  
+  handleClick() {
+    this.setState((prevState => {
+      return {
+        video.resolution: preState.video.resolution - 360
+      }
+
+   })
+}
+render() {
+  return (
+    <div>
+       <h1>{console.log(this.props.settings.video.resolution}</h1>
+       <button onClick={this.handleClick}>Current Value</button>
+    </div>
+   )
+   }
+}
+
